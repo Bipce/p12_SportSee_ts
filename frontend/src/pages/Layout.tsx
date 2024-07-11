e538818560f38c3f9db6a206040b30f78fbbfab3
Claire Royer <claireroyer57@gmail.com>
2024-07-11 17:27:19 +0200

    Fix height of container

--- frontend/src/pages/Layout.tsx
+++ frontend/src/pages/Layout.tsx
@@ -4,13 +4,13 @@ import { Outlet } from "react-router-dom";
 
 const Layout = () => {
   return (
-    <div className="main-container">
+    <>
       <HeaderNavbar />
       <section className="container">
         <LateralNavbar />
         <Outlet />
       </section>
-    </div>
+    </>
   );
 };
 
